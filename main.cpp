34f107299ccba4b25b050e7f9c162032d5d61001
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-03-04 21:33:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,43 @@
 #include <algorithm>
-#include <cstring>
 #include <iostream>
-#include <vector>
-#include "Polynomial.hpp"
+#include <iterator>
+#include <functional>
 
 int main()
 {
-    std::vector<double> vec = {1, 2, 1};
-    std::vector<double> vec1 = {5, 2, 6, 0, 1};
-    Polynomial <double>polyOfVec(vec);
-    Polynomial <double>poly;
-    Polynomial <double>polyOfVec1(vec1);
-    std::cout << polyOfVec1(1.)<< std::endl;
-    polyOfVec1 /= polyOfVec;
-    polyOfVec1 *= polyOfVec;
-    polyOfVec1 %= polyOfVec;
-    poly = polyOfVec1 % polyOfVec;
-    polyOfVec += polyOfVec1;
-    polyOfVec -= polyOfVec1;
-    polyOfVec1 = polyOfVec + polyOfVec;
-    polyOfVec = polyOfVec1 - polyOfVec;
-    polyOfVec1 *= polyOfVec;
-    poly = polyOfVec * 4.;
-    poly = 7. * poly;
-    std::cout << (polyOfVec1 == polyOfVec) << std::endl;
-    std::cout << (polyOfVec1 != polyOfVec) << std::endl;
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<> dis(0, 9);
+    std::vector<int> v1(10), v2(10), v3(10), v4(10);
+    std::generate(v1.begin(), v1.end(), std::bind(dis, std::ref(mt)));
+    std::generate(v2.begin(), v2.end(), std::bind(dis, std::ref(mt)));
+    std::generate(v3.begin(), v3.end(), std::bind(dis, std::ref(mt)));
+    std::generate(v4.begin(), v4.end(), std::bind(dis, std::ref(mt)));
+    std::cout << "v1 : ";
+    std::copy(v1.begin(), v1.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    merge_sort(v1.begin(), v1.end());
+    std::copy(v1.begin(), v1.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "v2 : ";
+    std::copy(v2.begin(), v2.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    quick_sort(v2.begin(), v2.end());
+    std::copy(v2.begin(), v2.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "v3 : ";
+    std::copy(v3.begin(), v3.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    insertion_sort(v3.begin(), v3.end());
+    std::copy(v3.begin(), v3.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "v4 : ";
+    std::copy(v4.begin(), v4.end(), std::ostream_iterator<int>(std::cout, " "));
+    heap_sort(v1.begin(), v1.end());
+    std::cout << std::endl;
+    std::copy(v4.begin(), v4.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::vector<int> sumVec;
+    merge(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(sumVec));
+    std::copy(sumVec.begin(), sumVec.end(), std::ostream_iterator<int>(std::cout, " "));
     return 0;
 }
